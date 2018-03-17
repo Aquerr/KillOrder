81f8d1fde3c0d3d3889f0d75f53d9b37f6788a0b
Aquerr <nerdianin@gmail.com>
2018-03-17 16:30:05 +0100

    Created project

---
+++ src/main/java/io/aquerr/killorder/PluginPermissions.java
@@ -0,0 +1,14 @@
+package io.aquerr.killorder;
+
+/**
+ * Created by Aquerr on 2018-02-15.
+ */
+public abstract class PluginPermissions
+{
+    public static final String HELP_COMMAND = "killorder.help";
+
+    public static final String SELECT_COMMAND = "killorder.create";
+    public static final String LIST_COMMAND = "killorder.list";
+
+    public static final String VERSION_NOTIFY = "killorder.version.notify";
+}
